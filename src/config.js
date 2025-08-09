86eeca0418d25b1c4415a8a37c4d72dfa9033f89
avirupThinkbridge <50024464+avirup2512@users.noreply.github.com>
2025-08-09 22:27:20 +0530

    chnage the config base url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    // baseUrl : "http://65.108.57.224:8089/api/"
-    baseUrl : "http://localhost:8089/"
+    baseUrl : "http://65.108.57.224:8089/api/"
+    // baseUrl : "http://localhost:8089/"
 }
 export default config;
