53f2c3a9a5202def2ecfeb2f20cc591ce8cfc78b
avirupThinkbridge <50024464+avirup2512@users.noreply.github.com>
2025-08-18 15:10:48 +0530

    change the api url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // baseUrl : "http://65.108.57.224:8089/api/"
     // baseUrl : "http://localhost:8089/"
-    baseUrl: "https://freelancecollab.com:8089/api/"
+    baseUrl: "https://freelancecollab.com/api/"
 }
 export default config;
